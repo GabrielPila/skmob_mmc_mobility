3d6366802cf2663a1d72dd81c7f6a0f3171c3da6
GabrielPila <gabriel.pilah@pucp.edu.pe>
2022-03-10 00:59:39 -0500

    downloading script implemented

---
+++ programs/01.2_s3_download_data.py
@@ -0,0 +1,16 @@
+from gan_utils.s3_utils import download_file_from_s3
+from config import PATH_LOCAL_DATA, PATH_S3_DATA
+import os
+
+for path_dir in ['data','experiment']:
+    if not os.path.exists(path_dir):
+        os.mkdir(path_dir)
+
+
+for filename in ['geolife_sample.parquet', 'geolife_consolidated.parquet']:
+    download_file_from_s3(
+        filename=filename, 
+        local_path=PATH_LOCAL_DATA, 
+        s3_path=PATH_S3_DATA, 
+        print_progress=True
+    )
